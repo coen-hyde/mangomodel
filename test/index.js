07c5b460ea965b8171b47f876eb24ac3c6507e1a
Coen Hyde <coen.hyde@gmail.com>
2012-09-27 16:07:05 +1000

    refactored MangoModel to support global db setting

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 var should = require('should')
   , Monk = require('monk')
-  , _ = require('underscore')
 	, MangoModel = require('../lib/mangomodel.js')
   , fixtures = require('./data/mongo_fixtures.js')
   , ObjectId = require('mongodb/node_modules/bson').ObjectID;
@@ -10,13 +9,12 @@ var db = Monk('127.0.0.1/mangomodel-test');
 beforeEach(function(done) {
   fixtures.load(db, done);
   MangoModel.models = [];
+  delete MangoModel.globalDb
 });
 
 describe('MangoModel', function(){
   it('should be able to forward method calls to monk', function(done){
-    var Ants = new MangoModel(db, {
-      collection: 'ants'
-    });
+    var Ants = new MangoModel(db, 'ants');
 
     Ants.findById('aa0000000000000000000003', function(err, doc) {
       should.not.exist(err);
@@ -26,16 +24,40 @@ describe('MangoModel', function(){
     });
   });
   it('should be able to define model and retrieve it', function() {
-    MangoModel.create(db, {
-      collection: 'bugs'
-    });
+    MangoModel.create(db, 'bugs');
 
     var Bugs = MangoModel.model('bugs');
     should.exist(Bugs);
     Bugs.modelDefinition.should.have.property('collection', 'bugs');
   });
+  it('should be able to create a model with global db', function(done) {
+    MangoModel.models.should.have.length(0);
+    MangoModel.setDb(db);
+
+    var Ants = MangoModel.create('ants');
+    done();
+    // Ants.findById('aa0000000000000000000004', function(err, doc) {
+    //   should.not.exist(err);
+    //   doc._id.should.eql(ObjectId('aa0000000000000000000004'));
+    //   done();
+    // });
+  });
   it('should not be able to retrieve a model that does not exist', function() {
     var Worms = MangoModel.model('worms');
     should.not.exist(Worms);
+  });
+  it('should transfer methods from methods property onto model', function(done) {
+    MangoModel.create(db, 'cats', {
+      methods: {
+        echo: function() {
+          return this.modelDefinition.collection;
+        }
+      }
+    });
+
+    var Cats = MangoModel.model('cats');
+    Cats.should.have.property('echo');
+    Cats.echo().should.eql('cats');
+    done();
   });
 });
